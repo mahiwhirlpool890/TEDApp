4e91ffdf8e77c8fc24d8da0baf793e299e1e0837
VikramNaik <vikram_vinod_naik@whirlpool.com>
2017-09-08 18:18:40 +0530

    Initial Commit

---
+++ WhirlpoolWcloudBaseApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WhirlpoolWcloudBaseApp
+//
+//  Created by Vikram Naik on 08/09/17.
+//  Copyright © 2017 whirlpool. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
